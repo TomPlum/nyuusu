f84931fdbdc8fee055b13335834dc3ad463b5f7a
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-09-20 16:40:42 +0100

    Initialised Vite/React/TypeScript & Installed Hepburn

---
+++ src/hooks/useKatakana/types.ts
@@ -0,0 +1,3 @@
+export interface UseKatakanaProps {
+    english: string
+}
---
+++ src/hooks/useKatakana/useKatakana.ts
@@ -0,0 +1,11 @@
+import {UseKatakanaProps} from "./types.ts";
+import hepburn from 'hepburn'
+import {useMemo} from "react";
+
+const useKatakana = ({ english }: UseKatakanaProps) => {
+    return useMemo(() => {
+        return hepburn.toKatakana(english)
+    }, [english])
+}
+
+export default useKatakana
