8d2d4cfd7be85d0fb699259c4a7533362ffec537
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-09-22 17:52:53 +0100

    Added article skeleton component and added grid to loading screen

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,14 +1,28 @@
 import { LinearProgress } from "@mui/material"
 import styles from './Loading.module.scss'
 import { useTranslation } from "react-i18next"
+import ArticleSkeleton from "modules/Article/components/ArticleSkeleton"
+import NewsGrid from "components/NewsGrid"
 
 const Loading = () => {
   const { t } = useTranslation()
 
   return (
     <div className={styles.wrapper}>
-      <p className={styles.text}>{t('loading')}</p>
-      <LinearProgress color='info' variant='indeterminate' className={styles.loading} />
+      <div className={styles.floating}>
+        <p className={styles.text}>{t('loading')}</p>
+        <LinearProgress color='info' variant='indeterminate' className={styles.loading} />
+      </div>
+
+      <div className={styles.grid}>
+        <NewsGrid>
+          {[...Array(9)].map((_num, i) => {
+            return (
+              <ArticleSkeleton key={i} />
+            )
+          })}
+        </NewsGrid>
+      </div>
     </div>
   )
 }
