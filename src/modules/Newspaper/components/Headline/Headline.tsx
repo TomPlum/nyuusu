9da09df9dfc3d466993366384a65e9a63309dc3a
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-09-23 14:01:11 +0100

    User can now copy the text from the newspaper headline

--- src/modules/Newspaper/components/Headline/Headline.tsx
+++ src/modules/Newspaper/components/Headline/Headline.tsx
@@ -1,10 +1,23 @@
 import { HeadlineProps } from "modules/Newspaper/components/Headline/types.ts"
 import styles from './Headline.module.scss'
+import { ContentCopy } from "@mui/icons-material"
+import { useTranslation } from "react-i18next"
+import { useCallback } from "react"
 
 const Headline = ({ headline }: HeadlineProps) => {
+  const { t } = useTranslation('translation', { keyPrefix: 'newspaper.headline' })
+
+  const copy = useCallback(() => {
+    navigator.clipboard.writeText(headline)
+  }, [headline])
+
   return (
     <div className={styles.headline}>
       {headline}
+
+      <div className={styles.copy} title={t('copy')} onClick={copy}>
+        <ContentCopy />
+      </div>
     </div>
   )
 }
