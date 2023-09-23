333fb2e5241bd7d646c90e4097d1f18adeddb7fc
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-09-23 20:05:41 +0100

    Added route error to error page from react router

--- src/views/ErrorView/ErrorView.tsx
+++ src/views/ErrorView/ErrorView.tsx
@@ -1,7 +1,9 @@
 import styles from './ErrorView.module.scss'
 import { useTranslation } from "react-i18next"
+import { useRouteError } from "react-router-dom"
 
 const ErrorView = () => {
+  const error = useRouteError() as { message: string }
   const { t } = useTranslation('translation', { keyPrefix: 'error-page' })
 
   return (
@@ -13,6 +15,10 @@ const ErrorView = () => {
 
         <p className={styles.description}>
           {t('description')}
+        </p>
+
+        <p className={styles.error}>
+          {error.message}
         </p>
 
         <a href='/articles'>{t('help')}</a>
