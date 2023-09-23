a75a68b4b1e2a6ec6e48b17e9cb2fec4c97837e2
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-09-23 19:33:24 +0100

    Added not found page

--- src/router.tsx
+++ src/router.tsx
@@ -2,10 +2,12 @@ import { createBrowserRouter } from "react-router-dom"
 import CardsHeadlineView from "views/CardsHeadlineView"
 import SingleHeadlineView from "views/SingleHeadlineView"
 import NyuusuApplication from "./NyuusuApplication.tsx"
+import NotFoundView from "views/NotFoundView"
 
 export const router = createBrowserRouter([
   {
     element: <NyuusuApplication />,
+    errorElement: <NotFoundView />,
     children: [
       {
         path: '/articles',
