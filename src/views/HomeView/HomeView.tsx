480767b1632b1615e5d4ea8664496eafdb5fe88a
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-09-27 23:14:21 +0100

    Added dust component

--- src/views/HomeView/HomeView.tsx
+++ src/views/HomeView/HomeView.tsx
@@ -4,31 +4,36 @@ import { Dashboard, Newspaper } from "@mui/icons-material"
 import { useNavigate } from "react-router-dom"
 import styles from './HomeView.module.scss'
 import Grid from "@mui/material/Unstable_Grid2"
+import Dust from "views/HomeView/components/Dust"
 
 const HomeView = () => {
   const { t } = useTranslation('translation', { keyPrefix: 'views.home' })
   const navigate = useNavigate()
 
   return (
-    <Grid container className={styles.wrapper} sx={{ justifyContent: 'center' }} spacing={5}>
-      <Grid xs={12} sm={6} className={styles.left}>
-        <ViewTile
-          Icon={Newspaper}
-          desc={t('newspaper.desc')}
-          title={t('newspaper.title')}
-          onClick={() => navigate('/newspaper')}
-        />
-      </Grid>
+    <div className={styles.view}>
+      <Dust />
 
-      <Grid xs={12} sm={6} className={styles.right}>
-        <ViewTile
-          Icon={Dashboard}
-          desc={t('articles.desc')}
-          title={t('articles.title')}
-          onClick={() => navigate('/articles')}
-        />
+      <Grid container className={styles.wrapper} sx={{ justifyContent: 'center' }} spacing={5}>
+        <Grid xs={12} sm={6} className={styles.left}>
+          <ViewTile
+            Icon={Newspaper}
+            desc={t('newspaper.desc')}
+            title={t('newspaper.title')}
+            onClick={() => navigate('/newspaper')}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} className={styles.right}>
+          <ViewTile
+            Icon={Dashboard}
+            desc={t('articles.desc')}
+            title={t('articles.title')}
+            onClick={() => navigate('/articles')}
+          />
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   )
 }
 
---
+++ src/views/HomeView/components/Dust/Dust.tsx
@@ -0,0 +1,24 @@
+import styles from './Dust.module.scss'
+import classNames from "classnames"
+
+const Dust = () => {
+  return (
+    <ul className={styles.scene} data-friction-x="0.03" data-friction-y="0.05">
+      <li className={classNames(styles.layer, styles.specks)} data-depth="0.1" />
+      <li className={classNames(styles.layer, styles['layer-1'])} data-depth="0.15">
+        <div className={classNames(styles.img, styles['img-1'])} />
+      </li>
+
+      <li className={classNames(styles.layer, styles['layer-2'])} data-depth="0.25">
+        <div className={classNames(styles.img, styles['img-2'])} />
+      </li>
+
+      <li className={classNames(styles.layer, styles['layer-3'])} data-depth="0.45">
+        <div  className={classNames(styles.img, styles['img-3'])} />
+      </li>
+    </ul>
+
+  )
+}
+
+export default Dust
