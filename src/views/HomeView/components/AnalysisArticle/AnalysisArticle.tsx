f7918ed1bfae3f4802334bf0a0e03c9314908b5f
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-10-15 20:13:07 +0100

    Started implementing kanji grade bar chart on home page

--- src/views/HomeView/components/AnalysisArticle/AnalysisArticle.tsx
+++ src/views/HomeView/components/AnalysisArticle/AnalysisArticle.tsx
@@ -1,32 +1,22 @@
 import styles from './AnalysisArticle.module.scss'
 import { AnalysisArticleProps } from "views/HomeView/components/AnalysisArticle/types.ts"
 import classNames from "classnames"
-import { useCallback, useEffect, useMemo, useState } from "react"
-import { Chart as ChartJS, ArcElement, ChartData } from 'chart.js'
+import { useCallback, useEffect, useState } from "react"
+import { Chart as ChartJS, ArcElement, ChartData, BarElement, CategoryScale, LinearScale } from 'chart.js'
 import ChartDataLabels, { Context } from 'chartjs-plugin-datalabels'
-import { Pie } from 'react-chartjs-2'
+import { Bar, Pie } from 'react-chartjs-2'
 import { useTranslation } from "react-i18next"
 import Typography from "components/Typography"
-import useLanguageStats from "modules/Article/hooks/useLanguageStats"
-import { DifficultyRating } from "modules/Article/hooks/useLanguageStats/types.ts"
-import { Equalizer } from "@mui/icons-material"
+import { Equalizer, School } from "@mui/icons-material"
 
-ChartJS.register(ArcElement, ChartDataLabels)
+ChartJS.register(ArcElement, BarElement, CategoryScale, LinearScale, ChartDataLabels)
 
 const AnalysisArticle = ({ className }: AnalysisArticleProps) => {
-  const [data, setData] = useState<ChartData<'pie'>>()
-  const { difficulty } = useLanguageStats({ input: 'text' })
+  const [pieData, setPieData] = useState<ChartData<'pie'>>()
+  const [barData, setBarData] = useState<ChartData<'bar'>>()
   const [animationDuration, setAnimationDuration] = useState(0)
-  const { t } = useTranslation('translation', { keyPrefix: 'views.home.articles.analysis' })
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const difficultyClass = useMemo(() => {
-    switch (difficulty) {
-      case DifficultyRating.BEGINNER: return styles.beginner
-      case DifficultyRating.INTERMEDIATE: return styles.intermediate
-      case DifficultyRating.EXPERT: return styles.expert
-    }
-  }, [difficulty])
+  const { t } = useTranslation('translation', { keyPrefix: 'views.home.articles.analysis' })
 
   const getRandomInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
@@ -38,11 +28,20 @@ const AnalysisArticle = ({ className }: AnalysisArticleProps) => {
     const katakana = getRandomInt(5, 90 - kanji - hiragana)
     const roman = 100 - kanji - hiragana - katakana
 
-    setData({
+    setBarData({
+      labels: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
+      datasets: [
+        {
+          data: Array(10).fill(0).map(() => getRandomInt(0, 8)),
+          backgroundColor: Array(10).fill('').map((_v, i) => `rgb(46,46,46,${1 - (i / 15)})`).reverse()
+        }
+      ]
+    })
+
+    setPieData({
       labels: ['字', 'ひ', 'カ', 'A'],
       datasets: [
         {
-          label: 'Test',
           data: [kanji, hiragana, kanji, roman],
           backgroundColor: [
             'rgb(46,46,46,1)',
@@ -69,6 +68,36 @@ const AnalysisArticle = ({ className }: AnalysisArticleProps) => {
 
   return (
     <div className={classNames(styles.wrapper, className)}>
+      <div className={styles.barWrapper}>
+        {barData && (
+          <Bar
+            id='grade-bar'
+            data={barData}
+            className={styles.bar}
+            options={{
+              responsive: true,
+              maintainAspectRatio: false,
+              animation: { duration: animationDuration },
+              scales: {
+                y: {
+                  display: false,
+                  grid: {
+                    display: false
+                  }
+                }
+              }
+            }}
+          />
+        )}
+
+        <div className={styles.barLabel}>
+          <School />
+          <span>
+            {t('bar.label')}
+          </span>
+        </div>
+      </div>
+
       <div className={styles.stats}>
         <div className={styles.heading}>
           <Equalizer className={styles.icon} />
@@ -109,9 +138,10 @@ const AnalysisArticle = ({ className }: AnalysisArticleProps) => {
       <div className={styles.pieContainer}>
         <div className={styles.outer}>
           <div className={styles.inner}>
-            {data && (
+            {pieData && (
               <Pie
-                data={data}
+                id='type-pie'
+                data={pieData}
                 // @ts-expect-error who knows?
                 plugins={[ChartDataLabels]}
                 className={styles.pie}
--- src/views/HomeView/components/AnalysisArticle/types.ts
+++ src/views/HomeView/components/AnalysisArticle/types.ts
@@ -1,3 +1,24 @@
 export interface AnalysisArticleProps {
     className?: string;
 }
+
+export type KanjiData = KanjiDatum[]
+
+export interface KanjiDatum {
+    "name": string,
+    "on": string[],
+    "kun": string[],
+    "source": string,
+    "meanings": string[],
+    "grade": number,
+    "jlpt": number,
+    "strokes": number,
+    "examples": KanjiExample[],
+    "tags": string[]
+}
+
+export interface KanjiExample {
+    value: string
+    kana: string[]
+    english: string[]
+}
