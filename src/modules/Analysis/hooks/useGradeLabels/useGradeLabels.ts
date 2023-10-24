6d906ce89fe99904ce7b846183c6d53ef6c3efbe
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-10-24 17:52:57 +0100

    Memoized bar chart labels to prevent infinite re-render loop

--- src/modules/Analysis/hooks/useGradeLabels/useGradeLabels.ts
+++ src/modules/Analysis/hooks/useGradeLabels/useGradeLabels.ts
@@ -1,9 +1,12 @@
 import { GradeLabelsResponse } from "modules/Analysis/hooks/useGradeLabels/types.ts"
 import { useTranslation } from "react-i18next"
+import { useMemo } from "react"
 
 const useGradeLabels = (): GradeLabelsResponse => {
   const { t } = useTranslation('translation', { keyPrefix: 'views.home.articles.analysis' })
-  const barChartLabels: string[] = t('bar.labels', { returnObjects: true })
+  const barChartLabels: string[] = useMemo(() => {
+    return t('bar.labels', { returnObjects: true })
+  }, [t])
 
   return {
     labels: barChartLabels
