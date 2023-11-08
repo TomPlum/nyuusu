09fee1aecad77deeea04d41c7bd43a456ea594e5
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-11-08 18:15:02 +0000

    Added translations for torn page footer and made path algorithm dynamic

--- src/components/TornPaperFooter/TornPaperFooter.tsx
+++ src/components/TornPaperFooter/TornPaperFooter.tsx
@@ -4,21 +4,23 @@ import { useWindowSize } from '@uidotdev/usehooks'
 import { GitHub, Help } from "@mui/icons-material"
 import classNames from "classnames"
 import { Link } from "react-router-dom"
+import { useTranslation } from "react-i18next"
 
 const TornPaperFooter = () => {
   const { width } = useWindowSize()
+  const { t } = useTranslation('translation', { keyPrefix: 'footer' })
 
   const getRandomInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min
   }
     
   const path = useMemo(() => {
-    const step = Math.floor((width ?? 1000) / 8)
-    const quantity = step > 100 ? 100 : step
+    const step = width === null || width < 1000 ? 4 : 2
+    const quantity = width == null ? 75 : width > 1000 ? 100 : 50
 
     const values = Array(quantity).fill('').map((_v, i) => {
-      const depth = getRandomInt(4, 12)
-      return `${i}% ${depth}px`
+      const depth = getRandomInt(4, 20)
+      return `${(i + 1) * step}% ${depth}px`
     })
 
     values.unshift('0% 0%') // <-- Top left
@@ -37,7 +39,7 @@ const TornPaperFooter = () => {
         <div className={classNames(styles.projects, styles.section)}>
           <p className={styles.paragraph}>
             <GitHub className={styles.github} />
-            <span>Related projects</span>
+            <span>{t('sections.related-projects')}</span>
           </p>
 
           <ul className={styles.list}>
@@ -49,11 +51,15 @@ const TornPaperFooter = () => {
         <div className={classNames(styles.help, styles.section)}>
           <p className={styles.paragraph}>
             <Help className={styles.helpIcon} />
-            <span>Help</span>
+            <span>{t('sections.help')}</span>
           </p>
 
           <ul className={styles.list}>
-            <li><Link to='https://github.com/TomPlum/learn-japanese'>Anki Configuration</Link></li>
+            <li>
+              <Link to='https://github.com/TomPlum/learn-japanese'>
+                {t('help.anki')}
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
