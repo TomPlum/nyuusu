93df4842507d0cf6c6305c85c7312e1ce1e398bc
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-11-23 18:44:33 +0000

    Fixed deepl backend by switching to fetch API

--- backend/translate.ts
+++ backend/translate.ts
@@ -1,5 +1,3 @@
-import https, { RequestOptions } from "https"
-
 export interface DeeplTranslationRequest {
     text: string[]
     source_lang: string
@@ -7,47 +5,17 @@ export interface DeeplTranslationRequest {
 }
 
 const callDeeplApi = (request: DeeplTranslationRequest) => {
-  const requestBody = JSON.stringify(request)
-
-  const options: RequestOptions = {
+  return fetch('https://api-free.deepl.com/v2/translate', {
     method: 'POST',
-    hostname: 'api-free.deepl.com',
-    path: '/v2/translate',
+    body: JSON.stringify(request),
     headers: {
       'Content-Type': 'application/json',
-      'Content-Length': requestBody.length,
       Authorization: `DeepL-Auth-Key ${process.env.DEEPL_API_KEY}`
     }
-  }
-
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, res => {
-      let rawData = ''
-
-      res.on('data', chunk => {
-        rawData += chunk
-      })
-
-      res.on('end', () => {
-        try {
-          resolve(JSON.parse(rawData))
-        } catch (err) {
-          reject(new Error(String(err)))
-        }
-      })
-    })
-
-    req.on('error', err => {
-      reject(new Error(String(err)))
-    })
-
-    req.on('timeout', () => {
-      req.destroy()
-      reject(new Error('Request time out'))
-    })
-
-    req.write(requestBody)
-    req.end()
+  }).then((response) => {
+    return response.json()
+  }).catch((e) => {
+    console.error(e)
   })
 }
 
