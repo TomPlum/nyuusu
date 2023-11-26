8685e57d738414b5812755735224ecfb68bf00d3
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-11-26 13:08:17 +0000

    Added vercel API client hook and switched headlines hook over to it

--- src/api/hooks/useGetLatestHeadlines/useGetLatestHeadlines.handlers.ts
+++ src/api/hooks/useGetLatestHeadlines/useGetLatestHeadlines.handlers.ts
@@ -2,7 +2,7 @@ import { RequestHandler, rest } from "msw"
 import { useGetLatestHeadlinesResponses } from "api/hooks/useGetLatestHeadlines/useGetLatestHeadlines.responses.ts"
 
 export const useGetLatestHeadlinesHandlers: RequestHandler[] = [
-  rest.get('*/newscatcher-api-headlines', (_req, res, ctx) => {
+  rest.get('*/news', (_req, res, ctx) => {
     return res(ctx.json(useGetLatestHeadlinesResponses))
   })
 ]
--- src/api/hosts.ts
+++ src/api/hosts.ts
@@ -7,6 +7,7 @@ export interface ApiHosts {
   newsCatcherApi: string
   anki: string
   deepl: string
+  vercel: string
 }
 
 export const hosts: Record<string, ApiHosts> = {
@@ -18,7 +19,8 @@ export const hosts: Record<string, ApiHosts> = {
     japanTimesRss: '',
     asahi: '',
     anki: 'http://localhost:8765',
-    deepl: 'https://local-deepl-api'
+    deepl: 'https://local-deepl-api',
+    vercel: 'https://nyuusu-git-dev-tomplums-projects.vercel.app'
   },
   production: {
     newsApi: 'https://newsapi.org',
@@ -28,6 +30,7 @@ export const hosts: Record<string, ApiHosts> = {
     asahi: 'https://rss.asahi.com/rss/asahi',
     newsCatcherApi: import.meta.env.NEWSCATCHER_API_HOST,
     anki: '127.0.0.1:8765',
-    deepl: 'https://api-free.deepl.com'
+    deepl: 'https://api-free.deepl.com',
+    vercel: 'https://nyuusu.vercel.app'
   }
 }
