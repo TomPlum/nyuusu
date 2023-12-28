272f3329324b6fca83401e39e81a9e8e67b6e531
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-28 13:33:53 +0000

    Added console log and printed msw handlers to debug failing test on CI

--- backend/api/news/news.spec.ts
+++ backend/api/news/news.spec.ts
@@ -1,11 +1,14 @@
 import api from '../../index'
 import request from "supertest"
 import {newsCatcherResponse} from "./news.responses";
+import {server} from "../../mocks/node";
 
 describe('News API', () => {
     it('should return the correct response', async () => {
         const response = await request(api).get('/news')
 
+        console.log('response', response)
+        server.listHandlers()
         expect(response.status).toBe(200)
 
         expect(response.body).toStrictEqual(newsCatcherResponse)
