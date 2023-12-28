c236a9966658feca5fc5e61af98701920f5dd1b7
Thomas Plumpton <Thomas.Plumpton@hotmail.co.uk>
2023-12-28 14:02:38 +0000

    Removed debug logging now that tests are fixed and added cypress E2E tests to dev workflow

--- backend/api/news/news.spec.ts
+++ backend/api/news/news.spec.ts
@@ -1,15 +1,11 @@
 import api from '../../index'
 import request from "supertest"
 import {newsCatcherResponse} from "./news.responses";
-import {server} from "../../mocks/node";
 
 describe('News API', () => {
     it('should return the correct response', async () => {
         const response = await request(api).get('/news')
 
-        console.log('response', response)
-        console.log('test api key', process.env.NEWSCATCHER_API_KEY)
-        server.listHandlers()
         expect(response.status).toBe(200)
 
         expect(response.body).toStrictEqual(newsCatcherResponse)
